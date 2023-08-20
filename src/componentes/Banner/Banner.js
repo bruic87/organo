bfe5d7d40c9da31009fc20911aee655ede747dc0
bruic87 <bruic@outlook.com>
2023-08-20 11:49:01 -0300

    'componente banner criado'

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,6 +1,10 @@
+import './Banner.css'
+
 function Banner() {
     return (
-        <img src="/imagens/banner.png" alt="O banner da principal da página do Organo"></img>
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="O banner da principal da página do Organo"></img>
+        </header>
     )
 }
 
